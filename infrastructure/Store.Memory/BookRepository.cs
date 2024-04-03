3100844a0bb00ebe84b4ed42ed1d451fe4271cf5
Alexey Franchuk <alexy.alexy98@mail.ru>
2024-04-04 00:47:39 +0300

    Create controller/search create view search

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Книга карлсон"),
+            new Book(2, "Книга Золотой горшочек"),
+            new Book(3, "Книга Красавица и чудовище")
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
