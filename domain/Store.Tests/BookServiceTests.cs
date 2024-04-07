1ca0dfbf29c2ddc070a531af5623a20f23cf2f3c
Alexey Franchuk <alexy.alexy98@mail.ru>
2024-04-07 19:28:10 +0300

    add-description create view for description

--- domain/Store.Tests/BookServiceTests.cs
+++ domain/Store.Tests/BookServiceTests.cs
@@ -13,9 +13,9 @@ namespace Store.Tests
         public void GetAllByQuery_WithIsbn_CallsGetAllByIsbn()
         {
             var bookRepositoryStub = new Mock<IBookRepository>();
-            bookRepositoryStub.Setup(x => x.GetAllByIsbn(It.IsAny<string>())).Returns(new[] { new Book(1, "", "", "") });
+            bookRepositoryStub.Setup(x => x.GetAllByIsbn(It.IsAny<string>())).Returns(new[] { new Book(1, "", "", "", "", 0m) });
 
-            bookRepositoryStub.Setup(x => x.GetAllByTitleOrAuthor(It.IsAny<string>())).Returns(new[] { new Book(2, "", "", "") });
+            bookRepositoryStub.Setup(x => x.GetAllByTitleOrAuthor(It.IsAny<string>())).Returns(new[] { new Book(2, "", "", "", "", 0m) });
 
             var bookService = new BookService(bookRepositoryStub.Object);
             var validIsbn = "ISBN 12345-67890";
@@ -29,9 +29,9 @@ namespace Store.Tests
         public void GetAllByQuery_WithAuthor_CallsGetAllByTitleOrAuthor()
         {
             var bookRepositoryStub = new Mock<IBookRepository>();
-            bookRepositoryStub.Setup(x => x.GetAllByIsbn(It.IsAny<string>())).Returns(new[] { new Book(1, "", "", "") });
+            bookRepositoryStub.Setup(x => x.GetAllByIsbn(It.IsAny<string>())).Returns(new[] { new Book(1, "", "", "", "", 0m) });
 
-            bookRepositoryStub.Setup(x => x.GetAllByTitleOrAuthor(It.IsAny<string>())).Returns(new[] { new Book(2, "", "", "") });
+            bookRepositoryStub.Setup(x => x.GetAllByTitleOrAuthor(It.IsAny<string>())).Returns(new[] { new Book(2, "", "", "", "", 0m) });
 
             var bookService = new BookService(bookRepositoryStub.Object);
             var invalidIsbn = "12345-67890";
--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -4,9 +4,9 @@
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "ISBN 12312-31231", "D. Knuth", "Книга карлсон"),
-            new Book(2, "ISBN 12312-31345", "A. Kalaghen",  "Книга Золотой горшочек"),
-            new Book(3, "ISBN 12312-31689", "U. Goldenfold", "Книга Красавица и чудовище")
+            new Book(1, "ISBN 12312-31231", "D. Knuth", "Книга карлсон", "description", 1500m),
+            new Book(2, "ISBN 12312-31345", "A. Kalaghen",  "Книга Золотой горшочек", "Description Description", 5500m),
+            new Book(3, "ISBN 12312-31689", "U. Goldenfold", "Книга Красавица и чудовище", "Descriiption 2", 6600m)
         };
 
         public Book[] GetAllByIsbn(string isbn)
@@ -17,6 +17,11 @@
         public Book[] GetAllByTitleOrAuthor(string query)
         {
             return books.Where(book => book.Author.Contains(query) || book.Title.Contains(query)).ToArray();
+        }
+
+        public Book GetById(int id)
+        {
+            return books.Single(book => book.Id == id);
         }
     }
 }
